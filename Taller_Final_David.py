c93cccb59251c3442e25247a5ba677bf2af66407
David Villa <davidvillasotomayor@gmail.com>
2021-11-30 10:09:59 -0500

    Ejercicio 2

--- Taller_Final_David.py
+++ Taller_Final_David.py
@@ -8,9 +8,24 @@ Created on Tue Nov 30 09:49:42 2021
 import pandas as pd
 
 url = 'covid_22_noviembre.csv'
-data = pd.read_csv(url )
+data = pd.read_csv(url)
 
 #Ejercicio 1
 
 num_casos = len(data)
 print (num_casos)
+
+#Ejercicio 2 
+
+data['Nombre municipio'].replace('puerto colombia','PUERTO COLOMBIA', inplace=True)
+data['Nombre municipio'].replace('puerto COLOMBIA','PUERTO COLOMBIA', inplace=True)
+data['Nombre municipio'].replace('MEDELLiN','MEDELLIN', inplace=True)
+data['Nombre municipio'].replace('Galapa','GALAPA', inplace=True)
+data['Nombre municipio'].replace('momil','MOMIL', inplace=True)
+data['Nombre municipio'].replace('Guepsa','GUEPSA', inplace=True)
+data['Nombre municipio'].replace('barrancabermeja','BARRANCABERMEJA', inplace=True)
+data['Nombre municipio'].replace('Pensilvania','PENSILVANIA', inplace=True)
+data['Nombre municipio'].replace('Anserma','ANSERMA', inplace=True)
+
+num_municipios = len(data.groupby('Nombre municipio').size())
+print (num_municipios)
